324f3ac12e96a24ebd7ff392e04409f3c8e4d897
wojtek <wojtek@sp36501.onmicrosoft.com>
2019-12-16 21:06:05 +0100

    Przeniesienie plikow modelu do projektu Repozytorium

--- Repozytorium/Models/Kategoria.cs
+++ Repozytorium/Models/Kategoria.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace OGL.Models
+namespace Repozytorium.Models
 {
     public class Kategoria
     {
--- Repozytorium/Models/Uzytkownik.cs
+++ Repozytorium/Models/Uzytkownik.cs
@@ -9,7 +9,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace OGL.Models
+namespace Repozytorium.Models
 {
     public class Uzytkownik:IdentityUser
     {
